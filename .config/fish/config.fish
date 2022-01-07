fb81271e60bd393c1c913e2833f913b3d8bca68f
silipwn <contact@as-hw.in>
2022-01-07 12:01:19 -0500

    Misc changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,34 @@
+# cat ~/.cache/wal/sequences
+set PATH /home/silipwn/Code/code-ql/codeql /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl /home/silipwn/Developments/TChains/Go/bin /home/silipwn/.cargo/bin /home/silipwn/.local/bin /home/silipwn/Developments/Scripts /home/silipwn/.emacs.d/bin 
+# starship init fish | source
+set -gx GOPATH /home/silipwn/Developments/TChains/Go
+set -g QT_QPA_PLATFORMTHEME qt5ct
 set -g FORGIT_COPY_CMD 'xclip -selection clipboard'
-set -gx EDITOR vim
+# set -gx EDITOR nvim
 if status is-interactive
-   # https://www.markhansen.co.nz/auto-start-tmux/
-   if not set -q TMUX
+    # https://www.markhansen.co.nz/auto-start-tmux/
+    if not set -q TMUX
         read -l -P 'Attach to tmux [Name/l/m] ?' confirm
         switch $confirm
             case l L
-                tmux attach-session -t (tmux list-sessions -F '#{session_name}' | fzf)
+                tmux new-session -A -s (tmux list-sessions -F '#{session_name}' | fzf --print-query)
+            case ''
+                echo "As you wish, sir"
             case m main Main
                 tmux new-session -A -s main
             case '*'
                 tmux new-session -A -s $confirm
     end
-    end
-set d (date +%Y_%m_%d)
+end
+set d (date +%Y-%m-%d)
 set t (date +%H:%M)
 function meet
     # Easy noting stuff
     set file "$HOME/Dropbox/LogSeq/pages/Meeting-$d-$argv[1]-$argv[2].org"
-    echo "* Meeting with $argv[1] about $argv[2] on "(date) >> $file
+    echo "* Meeting with $argv[1] for $argv[2] on "(date +'%a %d %b %Y') >> $file
     vim "$file"
     echo "Edited $file"
-    end
+end
 function note
     # Wrapper function for instant markdown notes
     set file $HOME/Dropbox/LogSeq/journals/$d.org
@@ -41,3 +48,4 @@ function todo
     echo "Added note to $file"
 end
 end
+source ~/.asdf/asdf.fish
