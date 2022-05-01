b333b47a77a89b1ad7dcda814528250571f2e286
silipwn <contact@as-hw.in>
2022-05-01 14:32:43 -0400

    Add pandoc templates

---
+++ Developments/Scripts/template.md
@@ -0,0 +1,17 @@
+---
+title:  'This is the title: it contains a colon'
+author:
+- Author One
+- Author Two
+keywords: [nothing, nothingness]
+abstract: |
+  This is the abstract.
+
+  It consists of two paragraphs.
+---
+
+# Hello
+## Notes
+1. Test
+2. Wow
+> bash script.sh
--- dot_config/private_fish/executable_config.fish
+++ dot_config/private_fish/executable_config.fish
@@ -8,7 +8,7 @@ set -g FORGIT_COPY_CMD 'xclip -selection clipboard'
 if status is-interactive
     # https://www.markhansen.co.nz/auto-start-tmux/
     if not set -q TMUX
-        read -l -P 'Attach to tmux [Name/l/m] ?' confirm
+        read -l -P 'Attach to tmux [Name/l/m] ? ' confirm
         switch $confirm
             case l L
                 # https://github.com/junegunn/fzf/issues/1693
