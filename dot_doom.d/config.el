84e120fa1b237a6c07a191b32128627e1de52cb0
silipwn <contact@as-hw.in>
2022-06-16 19:38:07 -0400

    Add doomstuff for bk

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -2,8 +2,6 @@
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
-
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "silipwn"
