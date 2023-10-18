92ced77e8cb44a8bd0e9160f1fa6d34759a1bd8d
silipwn <contact@as-hw.in>
2023-10-18 09:11:04 -0400

    Fix bugs in mpv spawn

--- dot_local/custom/bin/executable_mpv-spawn.sh
+++ dot_local/custom/bin/executable_mpv-spawn.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-notify-send -t 5000 -i '/usr/share/icons/Adwaita/32x32/mimetypes/video-x-generic-symbolic.symbolic.png' \
-"Playing in Spawned U-MPV" "`xclip -o -sel clip | xargs yt-dlp -e`"
-
+# Check if there's a URL in the clipboard
+content=`xclip -o sel clip`
+if [[ "$content" == http* ]]; then
+  notify-send -t 5000 -i '/usr/share/icons/Adwaita/32x32/mimetypes/video-x-generic-symbolic.symbolic.png' \
+  "Playing in Spawned U-MPV" ""
 # plays the link in mpv
-~/.local/bin/umpv `xclip -o -sel clip`
+  ~/.local/bin/umpv `xclip -o -sel clip`
+
+fi
